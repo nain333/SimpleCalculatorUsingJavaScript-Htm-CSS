1eea61ed8369692fca195abe75f886f8db78c8ae
@nain333 <hnain70@gmail.com>
2022-12-14 18:28:31 +0530

    Add JavaScript

--- calculator.js
+++ calculator.js
@@ -2,6 +2,29 @@ let display=document.getElementById('display');
 let buttons=Array.from(document.getElementsByClassName('button'));
 buttons.map(button=>{
     button.addEventListener('click', (e)=>{
+        switch(e.target.innerText){
+            case 'c':
+                display.innerText='';
+                break;
+            case '←':
+                if(display.innerText){ // to avoid unnecessary funciton calls.
+                    display.innerText=display.innerText.slice(0,-1); 
+                }
+                break;
+                case '=':
+                    try{
+                        display.innerText=eval(display.innerText);
+                         
+                    }catch{
+                        display.innerHTML='Error!'
+                    }
+                   
+                    break;
+
+
+            default:
+                display.innerText+=e.target.innerText;  
+        }
     
 
     });
